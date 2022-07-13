46565336efe8a3ef69e40852a0acedbd5679196c
Leandro Janotte <ljanotte.dev@gmail.com>
2022-07-12 23:22:35 -0300

    useState

---
+++ frontend/src/components/Exemplo/index.tsx
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+function Header() {
+    return (
+        <>
+            <div className="dsmeta-">
+                <h1>Meu componente</h1>
+            </div>
+        </>
+    );
+}
+
+export default Header;
